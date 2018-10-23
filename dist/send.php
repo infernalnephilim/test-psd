a4d547b45d2ad169731343d3d98955b712c9c44a
Aleksandra Półtorak <xmacabrax@gmail.com>
2018-10-23 21:51:40 +0200

    Dodanie "lang"

--- dist/send.php
+++ dist/send.php
@@ -4,7 +4,7 @@
 	$name = $_POST['nameV'] ;
 	$surname = $_POST['surnameV'] ;
 	$telephone = $_POST['telephoneV'] ;
-	$message = "Proszę o kontakt w celu umówienia spotkania.\n Imię i nazwisko: " .$name. " " .$surname."\n E-Mail: " .$email. "\n Telefon: " .$telephone. "  ";
+	$message = " Proszę o kontakt w celu umówienia spotkania.\n Imię i nazwisko: " .$name. " " .$surname."\n E-Mail: " .$email. "\n Telefon: " .$telephone. "  ";
 
 	mail("aleksandra.poltorak.x@gmail.com", "E-Mail od: " .$name. " " .$surname." z formularza kontaktowego", $message, "From:" . $email);
 
